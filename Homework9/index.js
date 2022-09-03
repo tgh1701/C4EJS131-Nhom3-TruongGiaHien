42e592cdc27be48b3dd1b695bd7bd38ddbb37fae
Hien Truong <tgh1701@gmail.com>
2022-09-03 23:30:47 +0700

    Homework

--- Homework9/index.js
+++ Homework9/index.js
@@ -1,19 +1,19 @@
 // 1. Cho mảng A = [5, 4, 7, 8, 10, 20, 1], output -> [1, 4, 5, 7, 8, 10, 20] (hint: sử dụng hàm Array.sort)
 const arrA = [5, 4, 7, 8, 10, 20, 1];
-function sortNumbers(){
-    arrA.sort((a, b) => a - b );
+function sortNumbers() {
+  arrA.sort((a, b) => a - b);
 }
 sortNumbers();
 console.log("arrA = ", arrA);
 
-// 2. Cho mảng B = [1, 2, 'a', {}, 'b', 'c', 2 , 4, 5], output -> mảng C = [1,2,2,4,5] 
-const arrB = [1, 2, 'a', {}, 'b', 'c', 2, 4, 5];
+// 2. Cho mảng B = [1, 2, 'a', {}, 'b', 'c', 2 , 4, 5], output -> mảng C = [1,2,2,4,5]
+const arrB = [1, 2, "a", {}, "b", "c", 2, 4, 5];
 const arrC = [];
-const display = (value) => { 
-    if(Number.isInteger(value)){
-        arrC.push(value);
-    }
-}
+const display = (value) => {
+  if (Number.isInteger(value)) {
+    arrC.push(value);
+  }
+};
 arrB.forEach(display);
 console.log("arrC = ", arrC);
 
@@ -24,24 +24,23 @@ console.log("min = ", Math.min(...arrD));
 
 // 4. Cho mảng E = [1, 2, 'a', 'b', 'c', 2, 4, 5], không sử dụng thêm mảng mới, đưa các số về đầu mảng và các chữ về cuối mảng, sử dụng các hàm pop, push, shift, unshift
 // Cach 1
-const arrE1 = [1, 2, 'a', 'b', 'c', 2, 4, 5];
+const arrE1 = [1, 2, "a", "b", "c", 2, 4, 5];
 arrE1.splice(2, 3); // at position 2, remove 3 items
-arrE1.push('a', 'b', 'c');
-console.log("arrE = ", arrE1)
+arrE1.push("a", "b", "c");
+console.log("arrE = ", arrE1);
 
 // Cach 2
-const arrE2 = [1, 2, 'a', 'b', 'c', 2, 4, 5];
-for(let i = 3; i >= 1; i--){
-    arrE2.unshift(arrE2.pop());
+const arrE2 = [1, 2, "a", "b", "c", 2, 4, 5];
+for (let i = 3; i >= 1; i--) {
+  arrE2.unshift(arrE2.pop());
 }
-console.log("arrE = ", arrE2)
+console.log("arrE = ", arrE2);
 
 // 5. Cho mảng F = [1, 23, 41, 5, 6, 1, 14, 66, 1124, 1, 1, 55, 15, 0], tìm ra vị trí thứ 4 của số 1 trong mảng (hint: dùng findIndex)
 // Cach 1
 const arrF = [1, 23, 41, 5, 6, 1, 14, 66, 1124, 1, 1, 55, 15, 0];
 let checkNumber1 = arrF.findIndex(
-    (arrF, checkNumber1) => arrF === 1 && checkNumber1 > 9
-    );
+  (arrF, checkNumber1) => arrF === 1 && checkNumber1 > 9
+);
 console.log("checkNumber1 = ", checkNumber1);
 // Cach 2
-
